051da9559c74a94ac6e22120b56d80067fda71ac
Viviana Hurtado <viviana.hurtado-antiporta@stu.ocsb.ca>
2025-03-18 05:04:16 +0000

    Added scopeOfvariables.cpp

---
+++ scopeOfVariables.cpp
@@ -0,0 +1,41 @@
+// Copyright 2025 Viviana Hurtado
+// Created by Viviana Hurtado
+// March 17, 2025
+// This program
+#include <iostream>
+
+// global variable
+int variableX = 25;
+
+void localVariable() {
+    // this shows what happens with local variables
+    int variableX = 10;
+    int variableY = 30;
+    int variableZ;
+
+    variableX = variableX + 1;
+    variableZ = variableX + variableY;
+
+    std::cout << "Local variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+void globalVariable() {
+    // this shows what happens with global variables
+    int variableY = 30;
+    int variableZ;
+
+    variableX = variableX + 1;
+    variableZ = variableX + variableY;
+
+    std::cout << "Local variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+int main() {
+    // this function calls local and global
+    localVariable();
+    globalVariable();
+
+    std::cout << "\nDone. " << std::endl;
+}
